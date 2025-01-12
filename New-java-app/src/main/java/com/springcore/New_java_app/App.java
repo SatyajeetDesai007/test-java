ee7bb43cd9ed258cb45775d8c115032692cb3364
Desaisd007 <satyajeetdesai31@gmail.com>
2025-01-12 20:09:20 +0530

    testing cicd

---
+++ New-java-app/src/main/java/com/springcore/New_java_app/App.java
@@ -0,0 +1,10 @@
+package com.springcore.New_java_app;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ New-java-app/src/main/java/com/springcore/New_java_app/main.java
@@ -0,0 +1,9 @@
+package com.springcore.New_java_app;
+
+public class main {
+	
+	 public static void main(String[] args) {
+	        System.out.println("Hello, World!");
+	    }
+
+}
